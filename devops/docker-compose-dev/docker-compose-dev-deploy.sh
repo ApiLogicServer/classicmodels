49a196a8f75188d64a4d8c34a2851c2943feae15
Val Huber <valjhuber@gmail.com>
2023-08-10 12:44:40 -0700

    disable security

--- devops/docker-compose-dev/docker-compose-dev-deploy.sh
+++ devops/docker-compose-dev/docker-compose-dev-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# typically run from ?
-# sh ./devops/docker-compose/docker-compose-dev-deploy.sh
+# these are just notes for copy/paste into portal - not to be run as script.
 
 # ref: https://learn.microsoft.com/en-us/azure/app-service/tutorial-multi-container-app
 # eg: https://github.com/Azure-Samples/multicontainerwordpress
@@ -22,13 +21,6 @@ az group create --name myResourceGroup --location "westus"
 az appservice plan create --name myAppServicePlan --resource-group myResourceGroup --sku S1 --is-linux
 
 # create docker compose app
-# pg ref:
-az container create --resource-group <resource group name> \
-   --name <container name> \
-   --image <docker hub registry name>/<repository name>:<version> \
-   --dns-name-label <container name> \
-   --ports 5656 5002 \
-   --environment-variables SECURITY_ENABLED=True PYTHONPATH=/app/ApiLogicProject APILOGICPROJECT_SQLALCHEMY_DATABASE_URI=postgresql://postgres:p@postgresql-container.centralus.azurecontainer.io:5432/postgres APILOGICPROJECT_SQLALCHEMY_DATABASE_URI_AUTHENTICATION=postgresql://postgres:p@postgresql-container.centralus.azurecontainer.io:5432/authdb APILOGICPROJECT_SECURITY_ENABLED=True
 az webapp create --resource-group myResourceGroup --plan myAppServicePlan --name classicmodels --multicontainer-config-type compose --multicontainer-config-file devops/docker-compose-dev/docker-compose-dev.yml
 
 az container show --resource-group myResourceGroup --name <container name> 
